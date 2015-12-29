ff96f7a0a5c4e019bf1facda0769c0444892c6e9
anirudt <anirudt@gmail.com>
2015-12-29 09:17:14 +0530

    add extension

--- lotr.sh
+++ lotr.sh
@@ -8,13 +8,27 @@ mkdir series/
 NUMBER_MINS=`ffprobe -i $1 -show_entries format=duration -v quiet -of csv="p=0"`
 NUMBER_MINS=$((${NUMBER_MINS%.*}/60))
 NUMBER_PARTS=$(($NUMBER_MINS/20+1))
+file=$1
+
+if [[ $1 == *$EXTENSION ]]
+then
+  EXTENSION="$EXTENSION"
+else
+  if [[ $1 == *.mp4 ]]
+  then
+    EXTENSION=".mp4"
+  else
+    EXTENSION=".avi"
+  fi
+fi
+echo "$EXTENSION file reported"
 
 for (( i=0; i<$NUMBER_PARTS; i++ ))
 do
   echo "$[ i/3 ]:$[ (i*20)%60 ]:00"
 
   # Copying twenty minute section and doing the math
-  ffmpeg -y -i $1 -vcodec copy -acodec copy -ss $[ i/3 ]:$[ (i*20)%60 ]:00 -t 00:20:00 series/out_$i.mkv
+  ffmpeg -y -i $1 -vcodec copy -acodec copy -ss $[ i/3 ]:$[ (i*20)%60 ]:00 -t 00:20:00 series/out_$i$EXTENSION
 done
 
 
@@ -30,17 +44,17 @@ do
 
     # Take from an already existing file, if we have input
     # Else randomize
-    ffmpeg -y -i series/out_$[ i-1 ].mkv -vcodec copy -acodec copy -ss 00:$[ ( 4 + j * 5 ) ]:00 -t 00:00:15 recap/out_$j.mkv
-    echo "file 'recap/out_$j.mkv'" >> list.txt
+    ffmpeg -y -i series/out_$[ i-1 ]$EXTENSION -vcodec copy -acodec copy -ss 00:$[ ( 4 + j * 5 ) ]:00 -t 00:00:15 recap/out_$j$EXTENSION
+    echo "file 'recap/out_$j$EXTENSION'" >> list.txt
   done
-  echo "file 'series/out_$[ i ].mkv'" >> list.txt
-  ffmpeg -y -f concat -i list.txt -vcodec copy -acodec copy -c copy series/outre_$i.mkv
+  echo "file 'series/out_$[ i ]$EXTENSION'" >> list.txt
+  ffmpeg -y -f concat -i list.txt -vcodec copy -acodec copy -c copy series/outre_$i$EXTENSION
   rm -rf list.txt
 done
-mv series/out_0.mkv series/outre_0.mkv
+mv series/out_0$EXTENSION series/outre_0$EXTENSION
 
 echo "Cleaning up.. "
 for(( i=1; i<$NUMBER_PARTS; i++ ))
 do
-  rm -rf series/out_$i.mkv
+  rm -rf series/out_$i$EXTENSION
 done
